6b42e3179c37c587d51063b311db1c19772d0787
Hugo Moragues <hugo.moragues@gmail.com>
2018-05-06 00:29:21 +0200

    init

---
+++ Tests/StringCalculatorTest.php
@@ -0,0 +1,19 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use App\StringCalculator;
+use PHPUnit\Framework\TestCase;
+
+class StringCalculatorTest extends TestCase
+{
+	public function foo()
+	{
+
+	}
+
+	public function bar()
+	{
+		
+	}
+}
